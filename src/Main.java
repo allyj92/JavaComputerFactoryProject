6057d4c749abc7803640bbeae94b4edc4b5cba54
eunjaelim <imeunjae361@gmail.com>
2024-05-15 19:35:17 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        ComputerCase theCase = new ComputerCase("2208","Dell","240");
+        Monitor theMonitor = new Monitor("27inch Beast","Acer",27,"2540 x 1440");
+        MotherBoard theMotherboard = new MotherBoard("BJ=200","Asus",4,6,"v2.44");
+        PersonalComputer thePC = new PersonalComputer("2208","Dell",theCase,theMonitor,theMotherboard);
+
+//        thePC.getMonitor().drawPixelAt(10,10,"red");
+//        thePC.getMotherBoard().loadProgram("Windows OS");
+//        thePC.getComputerCase().pressPowerButton();
+        thePC.powerUp();
+    }
+}
